0a9bac9668ef851884b101f1fbe4cbdfa4b1d7b9
Ben Serrette <bserrett@iu.edu>
2018-05-03 12:00:29 -0400

    Fleshes out endpoints.

--- middleware/api/mag_api.py
+++ middleware/api/mag_api.py
@@ -17,6 +17,10 @@ from flask import Flask, abort, request, jsonify, g, url_for, render_template, e
 from flask_cors import CORS
 import json
 
+def run_query(query):
+    query_id = 12
+    return query_id
+
 # initialization
 app = Flask(__name__,
             static_folder="../../frontend/dist/assets",
@@ -30,26 +34,49 @@ logger = logging.getLogger(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/search')
+@app.route('/search', methods=['POST'])
 def get_search():
-    return 501
+    logger.info('Trying /search endpoint...')
+    return_type = request.json.get('return_type')
+    years = request.json.get('years')
+    title = request.json.get('title')
 
-@app.route('/results/<query_id>')
+    query_id = run_query("some query")
+    return jsonify({'query_id': query_id}), 202
+
+@app.route('/results/<query_id>', methods=['GET'])
 def get_results(query_id):
+    logger.info('Trying /results/' + query_id + ' endpoint...')
+
+    #check filesystem for query_ids[query_id].json
+
     return 501
 
-@app.route('/references/<paper_id>')
+@app.route('/references/<paper_id>', methods=['GET'])
 def get_references(paper_id):
-    return 501
+    logger.info('Trying /references/' + paper_id + ' endpoint...')
 
-@app.route('/citations/<paper_id>')
+    query_id = run_query("some query")
+    return jsonify({'query_id': query_id}), 202
+
+@app.route('/citations/<paper_id>', methods=['GET'])
 def get_citations(paper_id):
-    return 501
+    logger.info('Trying /citations/' + paper_id + ' endpoint...')
 
-@app.route('/paper/<paper_id>')
+    query_id = run_query("some query")
+    return jsonify({'query_id': query_id}), 202
+
+@app.route('/paper/<paper_id>', methods=['GET'])
 def get_paper(paper_id):
-    return 501
+    logger.info('Trying /paper/' + paper_id + ' endpoint...')
 
-@app.route('/papers')
+    query_id = run_query("some query")
+    return jsonify({'query_id': query_id}), 202
+
+@app.route('/papers', methods=['POST'])
 def get_multiple_papers():
-    return 501
+    logger.info('Trying /papers endpoint...')
+    papers_array = request.json.get('paper_ids')
+
+    query_id = run_query("some query")
+    return jsonify({'query_id': query_id}), 202
